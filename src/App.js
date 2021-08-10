f4deeab669990bf34149f9bb9047fd70be049d53
Mehdih77 <mehdiwork77@gmail.com>
2021-08-10 13:18:17 +0430

    final project

--- src/App.js
+++ src/App.js
@@ -1,17 +1,62 @@
+import { useEffect, useState } from 'react';
 import './App.css';
-import Header from './components/Header/Header';
 import RecipeCard from './components/RecipeCard/RecipeCard';
 
 
 function App() {
+
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState('');
+  const [query, setQuery] = useState('');
+
+  const getData = async () => {
+    const response = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=${query}&app_id=${process.env.REACT_APP_RECIPE_ID}&app_key=${process.env.REACT_APP_RECIPE_KEY}`);
+    const data = await response.json();
+    setRecipes(data.hits);
+  }
+
+  useEffect(() => {
+    getData();
+  }, [query])
+
+  function handleChangeSearch(e) {
+    setSearch(e.target.value)
+  }
+
+  // for prevent sending to much request with any changes in seacrh state
+  function handleGetSearch(e) {
+    e.preventDefault();
+    setQuery(search);
+    setSearch('');
+  }
+
   return (
     <section className='container recipe-container'>
       <div className='row'>
-        <Header />
+      <header className='header'>
+            <div className='header-title-logo'>
+               <h2>Recipe App</h2>
+               <img src="./images/food-icon.png" alt="food icon recipe green" />
+            </div>
+            <div className='search-box'>
+               <form onSubmit={handleGetSearch}>
+                 <input value={search} onChange={handleChangeSearch} className='search-box-input' type="text" placeholder='Search your recipe...' />
+                 <button type='submit' className='search-box-btn'>Search</button>
+               </form>
+            </div>
+        </header>
       </div>
-      <div className='row'>
-        <RecipeCard />
-      </div>
+        <main className="row custom-main">
+        {recipes && recipes.map(recipe => (
+          <RecipeCard
+          key={recipe.recipe.label}
+          title={recipe.recipe.label}
+          img={recipe.recipe.image}
+          calories={recipe.recipe.calories}
+          ingredients={recipe.recipe.ingredients} />
+        ))
+        }
+        </main>
     </section>
   );
 }
--- src/components/RecipeCard/RecipeCard.js
+++ src/components/RecipeCard/RecipeCard.js
@@ -1,26 +1,22 @@
 import React from 'react';
 import './RecipeCard.css'
 
-export default function RecipeCard() {
+export default function RecipeCard({ title, img, calories, ingredients }) {
+    
     return (
-        <main className='my-4'>
-            <div className='row recipe-card-container'>
-            <div className='recipe-card col-md-5'>
-            <img src="https://foodloversmarket.co.za/wp-content/uploads/website_recipe_1140x641_27.05.20-2-992x558.jpg" alt="" />
-                <h4>Chicken</h4>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rerum incidunt repudiandae accusantium earum facere laboriosam, labore, dolorem ab error, consectetur dignissimos! Perspiciatis at quaerat accusantium magni explicabo, ea doloremque numquam!</p>
-            </div>
-            <div className='recipe-card col-md-5'>
-            <img src="https://foodloversmarket.co.za/wp-content/uploads/website_recipe_1140x641_27.05.20-2-992x558.jpg" alt="" />
-                <h4>Chicken</h4>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rerum incidunt repudiandae accusantium earum facere laboriosam, labore, dolorem ab error, consectetur dignissimos! Perspiciatis at quaerat accusantium magni explicabo, ea doloremque numquam!</p>
-            </div>
-            <div className='recipe-card col-md-6'>
-            <img src="https://foodloversmarket.co.za/wp-content/uploads/website_recipe_1140x641_27.05.20-2-992x558.jpg" alt="" />
-                <h4>Chicken</h4>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rerum incidunt repudiandae accusantium earum facere laboriosam, labore, dolorem ab error, consectetur dignissimos! Perspiciatis at quaerat accusantium magni explicabo, ea doloremque numquam!</p>
-            </div>
-            </div>
-        </main>
+        <div className='col-md-5 my-4 recipe-card-container'>
+                <div className='recipe-card'>
+                    <img
+                        src={img}
+                        alt="recipe food green"/>
+                    <h4>{title}</h4>
+                    <p className={`${calories}`> 2000 ? "calories-red" : "calories-green"}>Calories: {calories.toFixed(0)}</p>
+                    <ol>
+                        {ingredients.map(ingredient => (
+                            <li>{ingredient.text}</li>
+                            ))}
+                    </ol>
+                </div>
+        </div>
     )
 }
